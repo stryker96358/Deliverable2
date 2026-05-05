171482fb2583c1f4fa687a1944bc05ce29ec71fb
stryker96358 <155046276+stryker96358@users.noreply.github.com>
2026-05-04 20:20:35 -0400

    Finished Game Code also fixing some errors

--- Game.java
+++ Game.java
@@ -1,4 +1,5 @@
-import java.util.ArrayList;   
+import java.util.ArrayList; 
+import java.util.List;  
 /**
  * Write a description of class Game here.
  *
@@ -7,29 +8,60 @@ import java.util.ArrayList;
  */
 public class Game
 {
-    String name;
-    String genre;
-    String platform;
-    String ageRating;
-    double[] rating;
+    private String name;
+    private Genre genre;
+    private Platform platform;
+    private ageRating ageRating;
+    private List<Double> ratings;
     
     /**
      * Constructor for objects of class Game
      */
-    public Game()
+    public Game(String name, Genre genre, Platform platform, ageRating ageRating)
     {
-        
+        this.name = name;
+        this.genre = genre;
+        this.platform = platform;
+        this.ageRating = ageRating;
+        this.ratings = new ArrayList<>();
     }
     
-    public void getAverageRating() {
-        
+    public double getAverageRating() {
+        if (ratings.isEmpty()) return 0;
+        double sum = 0;
+        for (double r : ratings) {
+            sum += r;
+        }
+        return sum / ratings.size();
     }
     
-    public void addRating() {
-        
+    public void addRating(double rating) {
+        ratings.add(rating);
     }
     
-    public void getDetails() {
-        
+    public String getDetails() {
+        return name + " (" + genre + ", " + platform + ") Rating: " + getAverageRating();
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public Genre getGenre() {
+        return genre;
+    }
+    
+    @Override
+    public String toString () {
+        return getDetails();
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Game) {
+            Game other = (Game) obj;
+            return this.name.equalsIgnoreCase(other.name);
+        }
+        return false;
     }
 }
